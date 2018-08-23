44cf08b75165aaa3c223b8428e22d08d5d63c90d
Light-et <light.elitetech@gmail.com>
2018-08-23 19:27:53 +0700

    clear

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,37 +2,19 @@
 <div>
   <q-carousel v-for="(item,index) in types" :key="index" class="component-top" style="max-width: 700px;margin:auto; height:300px; margin-bottom: 5px;" >
     <q-carousel-slide :img-src="item.imageSrc">
-      <div class="q-display-1" style="color:white">Bring on the fruit</div>
+      <div class="q-display-1" style="color:white">{{item.title}}</div>
       <q-btn class="fixed-bottom-left" rounded color="white" size="1vw" text-color="black" style="margin:10px" >
         <q-item @click.native="show(item)">
           <q-item-main :label="item.label" />
         </q-item>
       </q-btn>
       <!-- popup modal -->
-          <q-modal v-model="getIsHideDetail" :content-css="{minWidth: '48vw', minHeight: '86vh'}">
-            <details-home type="detailsHome" :data="data"></details-home>
+          <q-modal v-model="getIsHideDetail" :content-css="{minWidth: '68vw', minHeight: '86vh'}">
+            <details-home type="detailsHome" :data="types[0]"></details-home>
           </q-modal>
        <!-- popup modal -->
     </q-carousel-slide>
   </q-carousel>
-  <!-- <q-carousel class="component-top" style="max-width: 700px;margin:auto; height:300px" >
-    <q-carousel-slide img-src="assets/component-mid.jpg">
-    <div class="q-subheading">Discover coffee</div>
-        <div style="color:white" class="q-display-1"> </div>
-      <q-btn class="fixed-bottom-left" rounded color="white" size="1vw" text-color="black" style="margin:10px">
-        <q-item
-          v-for="modal in types2"
-          :key="modal.label2"
-          @click.native="modal.show()"
-        >
-          <q-item-main :label="modal.label2" />
-        </q-item>
-      </q-btn>
-          <q-modal v-model="basicModal2" :content-css="{minWidth: '48vw', minHeight: '86vh'}">
-            <details-home type="detailsHome"></details-home>
-          </q-modal>
-    </q-carousel-slide>
-  </q-carousel> -->
   <q-carousel class="component-top" style="max-width: 700px;margin:auto; height:300px" >
     <q-carousel-slide img-src="assets/panel.jpg">
     </q-carousel-slide>
@@ -53,7 +35,6 @@ export default {
     return {
       basicModal: false,
       data: null,
-      // imageSrc: 'assets/component.jpg',
       types: [
         {
           label: 'Learn More',
